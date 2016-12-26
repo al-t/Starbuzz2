950c7fc88e7a2ebf8cc8eeb6b8ccb5344b1c34df
Алексей Тимохин <alexey@telemark-it.ru>
2016-12-26 11:05:13 +0300

    fixed package nameP

--- app/src/main/java/ru/javabreeze/hfad/starbuzz/TopLevelActivity.java
+++ app/src/main/java/ru/javabreeze/hfad/starbuzz/TopLevelActivity.java
@@ -3,8 +3,6 @@ package ru.javabreeze.hfad.starbuzz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import ru.jababreeze.hfad.starbuzz.R;
-
 public class TopLevelActivity extends AppCompatActivity {
 
     @Override
